e503f881e637382320a5aeea012db87bebe4d39d
Theodoros <83337690+theodorosP@users.noreply.github.com>
2022-07-25 13:52:04 -0400

    Create df_rows_columns.py

---
+++ df_rows_columns.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def rows_colums(dataframe):
+  column_1 = dataframe.iloc[:, 1]
+  row_1 = print(dataframe.iloc[1])
+  column1 = dataframe[:][1]
+  return column_1, row_1, column1
